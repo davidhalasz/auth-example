394be5c9e2e43b644a17147d50ae76b9531f592a
davidhalasz <davidhalasz22@gmail.com>
2023-02-22 14:04:50 +0100

    added validator to username

--- backend/routes/validators/authValidator.js
+++ backend/routes/validators/authValidator.js
@@ -1,6 +1,7 @@
 const {body} = require('express-validator');
 
 module.exports = [
+    body('username', 'The username must be at least 3 characters!').isLength({min: 3}),
     body('email', 'The email address is not valid!').isEmail(),
     body('email', 'The email field cannot be empty!').notEmpty(),
     body('password', 'The password must be at least 7 characters!').isLength({min: 7}),
